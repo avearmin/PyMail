c53320c99a8a69c97d441919ab174ec16960be04
Armin Radoncic <Arminradoncic1@gmail.com>
2023-08-07 16:58:20 -0400

    Remake GmailAPI class into GmailAuthenticator
    I have remade the GmailAPI class into the GmailAuthenticator class to
    reduce its responsibilities to getting an access token.
    
    In addition, I have also wired it together with the GoogleCredentialsManager
    to properly handle cases where a refresh token is present or not.

--- credentials_manager.py
+++ credentials_manager.py
@@ -1,72 +1,89 @@
-import os, pickle
+import os
+import pickle
+import json
 from cryptography.fernet import Fernet
 
 
 class CredentialsManager:
-    
     def __init__(self, domain: str):
-         self.code_dir = os.path.dirname(os.path.abspath(__file__))
-         self.credentials_path =  os.path.join(self.code_dir, "credentials", f"{domain}_credentials.bin")
-    
-    def encrypt(self, key: bytes, credentials: str) -> bytes:
+        self.code_dir = os.path.dirname(os.path.abspath(__file__))
+        self.credentials_path = os.path.join(
+            self.code_dir, "credentials", f"{domain}_credentials.bin"
+        )
+
+    def encrypt(self, key: bytes, credential: str) -> bytes:
         f = Fernet(key)
-        return f.encrypt(credentials.encode())
-    
-    def decrypt(self, key: bytes, credentials: bytes) -> str:
+        return f.encrypt(credential.encode())
+
+    def decrypt(self, key: bytes, credential: bytes) -> str:
         f = Fernet(key)
-        return f.decrypt(credentials).decode()
-    
+        return f.decrypt(credential).decode()
+
     def verify_credentials_dir(self):
         cred_dir = os.path.join(self.code_dir, "credentials")
         if not os.path.exists(cred_dir):
             os.mkdir(cred_dir)
 
-    def load_credentials(self, key: bytes) -> dict:
+    def import_credentials(self, key: bytes, path: str):
+        if not os.path.exists(path):
+            print("Credentials not found.")
+            return
+        credentials = self.load_credentials()
+        with open(path, "r") as json_file:
+            json_data = json.load(json_file)
+        credentials["client_id"] = self.encrypt(
+            key, json_data["installed"]["client_id"]
+        )
+        credentials["client_secret"] = self.encrypt(
+            key, json_data["installed"]["client_secret"]
+        )
+        self.save_credentials(credentials)
+
+    def load_credentials(self) -> dict:
         self.verify_credentials_dir()
         if not os.path.exists(self.credentials_path):
             credentials = {}
             return credentials
         with open(self.credentials_path, "rb") as file:
             credentials = pickle.load(file)
-        return self.decrypt(key, credentials)
-        
-    def save_credentials(self, key: bytes, credentials: str):
+        return credentials
+
+    def save_credentials(self, credentials: str):
         self.verify_credentials_dir()
-        encrypted_credentials = self.encrypt(key, credentials)
         with open(self.credentials_path, "wb") as file:
-            pickle.dump(encrypted_credentials, file)
-        
+            pickle.dump(credentials, file)
+
     def save_refresh_token(self, key: bytes, refresh_token: str):
-        credentials = self.load_credentials(key)
-        credentials["refresh_token"] = refresh_token
-        self.save_credentials(key, credentials)
-        
+        credentials = self.load_credentials()
+        credentials["refresh_token"] = self.encrypt(key, refresh_token)
+        self.save_credentials(credentials)
+
     def save_client_id(self, key: bytes, client_id: str):
-        credentials = self.load_credentials(key)
-        credentials["client_id"] = client_id
-        self.save_credentials(key, credentials)
-    
+        credentials = self.load_credentials()
+        credentials["client_id"] = self.encrypt(key, client_id)
+        self.save_credentials(credentials)
+
     def save_client_secret(self, key: bytes, client_secret: str):
-        credentials = self.load_credentials(key)
-        credentials["client_secret"] = client_secret
-        self.save_credentials(key, credentials)
-    
+        credentials = self.load_credentials()
+        credentials["client_secret"] = self.encrypt(key, client_secret)
+        self.save_credentials(credentials)
+
     def load_refresh_token(self, key: bytes) -> str:
-        credentials = self.load_credentials(self, key)
+        credentials = self.load_credentials()
         if "refresh_token" in credentials:
-            return credentials["refresh_token"]
-            
+            return self.decrypt(key, credentials["refresh_token"])
+
     def load_client_id(self, key: bytes) -> str:
-        credentials = self.load_credentials(self, key)
+        credentials = self.load_credentials()
         if "client_id" in credentials:
-            return credentials["client_id"]
-    
+            return self.decrypt(key, credentials["client_id"])
+
     def load_client_secret(self, key: bytes) -> str:
-        credentials = self.load_credentials(self, key)
+        credentials = self.load_credentials()
         if "client_secret" in credentials:
-            return credentials["client_secret"]
+            return self.decrypt(key, credentials["client_secret"])
+
 
 class GoogleCredentialsManager(CredentialsManager):
-    
     def __init__(self):
         super().__init__("google")
--- email_api/email_api_base.py
+++
@@ -1,8 +0,0 @@
-class EmailAPIBase:
-    """Base class for email service provider APIs."""
-
-    def __init__(self, email, client_id, client_secret, token_endpoint):
-        self.email = email
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.token_endpoint = token_endpoint
