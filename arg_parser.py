2a5e9ecef6b257cc5319f5beba3e88553d87aa1f
avearmin <arminradoncic1@gmail.com>
2023-08-22 19:02:42 -0400

    Create EmailListManager class
    This class was created to to ensure users enter the correct email.
    Before loging into the client, users use the 'add' command to add
    the email to a .txt file that contains a list of emails they'd like
    to use for the client. When users attempt to login using an email,
    it will be validated against emails in the file to ensure it is
    correct.
    
    This is necessary to prevent misspelling, entering the wrong email,
    etc. This list also allows the client to simultaneously login to
    multiple emails at the same time without having to enter multiple
    emails directly into the CLI every time they wish to use the
    client.

--- arg_parser.py
+++ arg_parser.py
@@ -1,14 +1,22 @@
 from argparse import ArgumentParser
 from email_service import EmailService
+from email_list_manager import EmailListManager
 
 class ArgParser:
     def __init__(self):
+        self.email_list_manager = EmailListManager()
         self.parser = ArgumentParser()
         self.subparsers = self.parser.add_subparsers(title='command', dest='command')
 
         self.handle_login_cmd()
+        self.handle_add_cmd()
+        self.handle_del_cmd()
     
     def create_email_service_obj_and_login(self, email):
+        email_list = self.email_list_manager.load_email_list()
+        if email not in email_list:
+            print(f'{email} not found. Use add command to add it.')
+            return
         self.service = EmailService(email)
         self.service.start()
 
@@ -18,6 +26,20 @@ class ArgParser:
             'email', type=str, help='an email you want to login to'
         )
         self.parser_login_cmd.set_defaults(func=self.create_email_service_obj_and_login)
+
+    def handle_add_cmd(self):
+        self.parser_login_cmd = self.subparsers.add_parser('add')
+        self.parser_login_cmd.add_argument(
+            'email', type=str, help='an email you want to add'
+        )
+        self.parser_login_cmd.set_defaults(func=self.email_list_manager.write_email_to_list)
+    
+    def handle_del_cmd(self):
+        self.parser_login_cmd = self.subparsers.add_parser('del')
+        self.parser_login_cmd.add_argument(
+            'email', type=str, help='an email you want to delete'
+        )
+        self.parser_login_cmd.set_defaults(func=self.email_list_manager.delete_email_from_list)
     
     def parse_arguments(self):
         args = self.parser.parse_args()
@@ -25,3 +47,10 @@ class ArgParser:
             if args.func == self.create_email_service_obj_and_login:
                 email = args.email
                 args.func(email)
+            elif args.func == self.email_list_manager.write_email_to_list:
+                email = args.email
+                args.func(email)
+            elif args.func == self.email_list_manager.delete_email_from_list:
+                email = args.email
+                args.func(email)
+            
